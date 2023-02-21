b1310eadb2822e9036b22a80f85adf66e428a7f5
Kofi Kwafo Awua <109091636+Kkwafo@users.noreply.github.com>
2023-02-21 19:51:41 -0300

    acceso de datos sql

---
+++ TpFinalKofi.WebApi/Controllers/ClienteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TpFinalKofi.WebApi.Controllers
+{
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[HttpGet]
+
+    }
+}
---
+++ tpkofi.AccesodeDatos/Repositories/IClienteRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TpFinalKofi.Dominio.Models;
+
+namespace TpFinalKofi.AccesodeDatos.Repositories
+{
+    public interface IClienteRepository
+    {
+     public  Cliente GetCliente(string nombre);
+     public List<Cliente> GetClientes();
+     public  List<Cliente> EliminarCliente(string nombre);
+     public List<Cliente> ModificarCliente(Cliente nombre);
+     public List<Cliente> InsertarCliente(Cliente cliente);
+
+    }
+}
