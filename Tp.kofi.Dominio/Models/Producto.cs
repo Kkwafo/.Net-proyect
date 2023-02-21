65b9ccb1d6a07edffa9b4197ef4bb96815ed22e2
Kofi Kwafo Awua <109091636+Kkwafo@users.noreply.github.com>
2023-02-21 19:47:47 -0300

    Add files via upload

---
+++ Tp.kofi.Dominio/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpFinalKofi.Dominio.Models
+{
+    public class Producto
+    {
+        private string Id { get; set; }
+        private string ?Name{ get; set; }
+        private string ?Description { get; set; }
+        private string ?Price { get; set; }
+
+    }
+}
--- Tp.kofi.consola/Program.cs
+++ Tp.kofi.consola/Program.cs
@@ -1,22 +1,57 @@
 ﻿using System.Runtime.CompilerServices;
 using TpFinalKofi.AccesodeDatos.Repositories;
+using TpFinalKofi.Dominio.Models;
 
 namespace Tp.kofi.consola
 {
     internal class Program
     {
-        public static inMemoryPersonarepository personasInMemory = new();
+        public static InMemoryClienteRepository clienteInMemory = new();
+        
+
         static void Main(string[] args)
         {
-            MostrarMenu();
+            /*MostrarMenu()*/;
+
+            var sqlRepository = new SqlClienteRepository();
+
+            var clientes = sqlRepository .GetClientes();
+            var cliente = sqlRepository.GetCliente("Kofi");
+
+            Console.WriteLine("Get Cliente");
+            foreach (var item in clientes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Get Cliente");
+            Console.WriteLine(cliente.ToString());
+
+            var clienteNuevo = new Cliente
+            {
+                Id = 1,
+                Apellido = "Kwafo Awua",
+                Nombre = "Kofi",
+                Ciudad = "Cordoba",
+                Edad = 30,
+                Email = "kofikwafoawua@gmail.com",
+                Nacionalidad = "Argentina",
+                FechaDeNacimiento = DateTime.Now
+            };
+            clientes = sqlRepository.InsertarCliente(clienteNuevo);
+            Console.WriteLine("Get New Client");
+            foreach(var item in clientes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            }
         }
+
 
         private static void MostrarMenu()
         {
             string? resp;
             do
             {
-
                 string menu = "1: Person Create \n" +
                               "2: Show Person \n" +
                               "3: Show all Persons \n" +
@@ -39,11 +74,11 @@ namespace Tp.kofi.consola
                         break;
                     case "3":
                         Console.WriteLine($"You select: {election}");
-                        var personas = personasInMemory.GetPersona();
+                         var clientes = clienteInMemory.GetCliente();
                         Console.WriteLine($"Person List: {election}");
-                        foreach (var persona in personas)
+                        foreach (var cliente in clientes)
                         {
-                            Console.WriteLine(persona.ToString());
+                            Console.WriteLine(cliente.ToString());
 
                         }
                         break;
