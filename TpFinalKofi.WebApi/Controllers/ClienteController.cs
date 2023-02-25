0c627d2edd1ab686b5f54e0eaeb6d892bb43ec5d
Kofi Kwafo Awua <109091636+Kkwafo@users.noreply.github.com>
2023-02-25 18:03:18 -0300

    Add files via upload

--- TpFinalKofi.WebApi/Controllers/ClienteController.cs
+++ TpFinalKofi.WebApi/Controllers/ClienteController.cs
@@ -1,15 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using System.Collections.Generic;
+
 namespace TpFinalKofi.WebApi.Controllers
 {
     [ApiController]
+    [Route("Clientes")]
     public class ClienteController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        //[HttpGet]
+        private readonly IClienteRepository _clienteRepository;
 
+        public ClienteController(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        [HttpGet]
+        [Route("GetClientes")]
+        public IActionResult GetClientes()
+        {
+            var clientes = _clienteRepository.ObtenerTodos();
+            return Ok(clientes);
+        }
+
+        [HttpPost]
+        [Route("InsertarCliente")]
+        public IActionResult InsertarCliente(Cliente cliente)
+        {
+            _clienteRepository.InsertarCliente(cliente);
+            return Ok(cliente);
+        }
+
+        [HttpPut]
+        [Route("ModificarCliente/{id}")]
+        public IActionResult ModificarCliente(int id, Cliente cliente)
+        {
+            var clienteExistente = _clienteRepository.ObtenerPorId(id);
+            if (clienteExistente == null)
+            {
+                return NotFound();
+            }
+
+            clienteExistente.Nombre = cliente.Nombre;
+            clienteExistente.Apellido = cliente.Apellido;
+            clienteExistente.Edad = cliente.Edad;
+            clienteExistente.Ciudad = cliente.Ciudad;
+            clienteExistente.Email = cliente.Email;
+            clienteExistente.Nacionalidad = cliente.Nacionalidad;
+            clienteExistente.FechaDeNacimiento = cliente.FechaDeNacimiento;
+
+            _clienteRepository.ModificarCliente(clienteExistente);
+
+            return Ok(clienteExistente);
+        }
+
+        [HttpDelete]
+        [Route("EliminarCliente/{id}")]
+        public IActionResult EliminarCliente(int id)
+        {
+            var clienteExistente = _clienteRepository.ObtenerPorId(id);
+            if (clienteExistente == null)
+            {
+                return NotFound();
+            }
+
+            _clienteRepository.EliminarCliente(clienteExistente);
+
+            return Ok();
+        }
     }
 }
---
+++ TpFinalKofi.WebApi/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿namespace TpFinalKofi.WebApi.Models
+{
+        public class Cliente
+        {
+            public int Edad { get; set; }
+            public string? Nombre { get; set; }
+            public string? Apellido { get; set; }
+            public int? Id { get; set; }
+            public string? Nacionalidad { get; set; }
+            public string? Email { get; set; }
+            public string? Ciudad { get; set; }
+            public DateTime FechaDeNacimiento { get; set; }
+            public override string ToString()
+            {
+                return $"Id {Id} -  Nombre: {Nombre} - Edad: {Edad} - FechaNacimiento: {FechaDeNacimiento}";
+            }
+
+        }
+    
+}
