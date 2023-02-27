6ca488ca267a1119198546678126873323821a21
Kofi Kwafo Awua <109091636+Kkwafo@users.noreply.github.com>
2023-02-27 00:14:47 -0300

    Add files via upload

--- Tp.kofi.Dominio/Models/Cliente.cs
+++ Tp.kofi.Dominio/Models/Cliente.cs
@@ -2,7 +2,7 @@
 {
     public class Cliente
     {
-        public int Edad { get; set; }
+        public int? Edad { get; set; }
         public string? Nombre { get; set; }
         public string? Apellido { get; set; }
         public int? Id { get; set; }
